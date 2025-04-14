a594c0380e8ee5759433ab9493022c65d0146354
Pedro Henrique Carvalho <100935698+Peehcs@users.noreply.github.com>
2025-04-14 18:41:17 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryOject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: true,
   });
 
   try {
